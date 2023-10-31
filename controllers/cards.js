cf4b506364b0ea4021655ca613fbc34dcfcb7748
Dima Danilchenko <switch57dima@yandex.ru>
2023-11-01 00:04:04 +0300

    Настройка

--- controllers/cards.js
+++ controllers/cards.js
@@ -63,7 +63,7 @@ module.exports.likeCard = (req, res, next) => {
 };
 
 module.exports.dislikeCard = (req, res, next) => {
-  Card.findByIdAndRemove(
+  Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },

3a1a963ac7a4ba932551460632dd5b1c66544a9c
Dima Danilchenko <switch57dima@yandex.ru>
2023-11-24 23:46:55 +0300

    cors

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { celebrate, Joi, errors } = require('celebrate');
+const cors = require('cors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(cors);
+app.use(cors());
 app.use(requestLogger);
 
 app.post('/signin', celebrate({

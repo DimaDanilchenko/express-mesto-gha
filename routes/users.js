b3cca1ed9acafc9885c7d50fe4aca3d4e7a07844
Dima Danilchenko <switch57dima@yandex.ru>
2023-11-25 21:16:46 +0300

    cors

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:_id', celebrate({
+router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().hex().length(24),
   }),
@@ -17,7 +17,9 @@ router.patch('/me', celebrate({
     about: Joi.string().min(2).max(30),
   }),
 }), updateProfile);
+
 router.get('/me', getProfile);
+
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().regex(/^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),

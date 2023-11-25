559429c17c9a3a1237d14b539a61fc143fbc1d5c
Dima Danilchenko <switch57dima@yandex.ru>
2023-11-25 16:11:23 +0300

    cors

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(cors);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
